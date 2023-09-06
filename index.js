d2ff1da735a4e7bd5fb4c05d3730c077a3e88143
Intel Core i9 9900K <140819896+not-core-i9@users.noreply.github.com>
2023-09-06 08:55:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const { Client, GatewayIntentBits, Collection } = require("discord.js");
-
-const loadEvents = require("./src/handlers/loadEvents");
-const loadCommands = require("./src/handlers/loadCommands");
+const { Client, GatewayIntentBits } = require("discord.js");
+const { CommandKit } = require("commandkit");
 
 require("dotenv/config");
 
@@ -14,9 +12,10 @@ const client = new Client({
     ],
 });
 
-client.commands = new Collection();
-
-client.login(process.env.TOKEN).then(() => {
-    loadEvents(client);
-    loadCommands(client);
+new CommandKit({
+    client,
+    commandsPath: `${__dirname}/src/commands`,
+    eventsPath: `${__dirname}/src/events`,
 });
+
+client.login(process.env.TOKEN);
