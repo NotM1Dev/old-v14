acbc39a08cfe8c4bfc3a86357734473a59580abc
M1 <140819896+m1-dev@users.noreply.github.com>
2023-09-26 07:31:46 +0300

    Update ready.js

--- src/events/ready/ready.js
+++ src/events/ready/ready.js
@@ -1,3 +1,9 @@
+const mongoose = require("mongoose");
+
 module.exports = (client) => {
     console.log(`${client.user.tag} is online.`);
+    
+    mongoose.connect(process.env.MONGODB)
+        .then(() => console.log("MongoDB connected"))
+        .catch(console.error);
 }
