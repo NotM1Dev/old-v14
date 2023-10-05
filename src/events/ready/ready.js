c565744e1648f0a29d338fc08c068133b9e9e43d
M1 <m1dev.d.ts@skiff.com>
2023-10-05 09:55:24 +0000

    Delete src directory

--- src/events/ready/ready.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = (client) => {
-    console.log(`${client.user.tag} is online.`);
-    
-    mongoose.connect(process.env.MONGODB)
-        .then(() => console.log("MongoDB connected"))
-        .catch(console.error);
-}
