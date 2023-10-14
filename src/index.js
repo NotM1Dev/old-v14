ffb697cb5934a1bfadcd0e012e074c45f87f5c7f
m1-dev <=>
2023-10-14 15:33:56 +0300

    add dev configuration

--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@ new CommandKit({
     client,
     commandsPath: `${__dirname}/commands`,
     eventsPath: `${__dirname}/events`,
+
+    devGuildIds: ['YOUR_DEV_GUILD_ID'],
+    devUserIds: ['YOUR_USER_ID']
 });
 
 module.exports = client;
